c306f1a302afae7cc7f19ef5ac00aa0a735e5e4a
Le Vinh Thien <ohmygodvt95@gmail.com>
2015-11-04 14:41:44 +0700

    helper string

---
+++ application/helpers/string_helper.php
@@ -0,0 +1,25 @@
+<?php 
+function string_short ($str){
+    $unicode = array(
+            '-'=>'\'|\"|\\|\||\!|\@|\#|\$|\%|\^|\&|\*|\(|\)|;'
+        );
+        
+       foreach($unicode as $nonUnicode=>$uni){
+            $str = preg_replace("/($uni)/i", $nonUnicode, $str);
+       }
+       while(strpos( $str," "))
+       {
+            $str=str_replace(" ", "-", $str);
+       }
+       
+    return strtolower($str);
+    }
+ function generateRandomString($length = 10) {
+        $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, strlen($characters) - 1)];
+        }
+        return $randomString;
+    }
+ ?>
