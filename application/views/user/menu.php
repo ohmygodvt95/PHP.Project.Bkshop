44215d183cfdeff75dc4f5b335232cd479ac2907
Le Vinh Thien <ohmygodvt95@gmail.com>
2015-11-04 14:37:33 +0700

    modun cho menu user

---
+++ application/views/user/menu.php
@@ -0,0 +1,11 @@
+<div class="col-sm-3">
+                        	<ul class="list-group">
+                        		<li class="list-group-item">Hi, <i><?php echo $this->session->userdata('email'); ?></i></li>
+                        		<li class="list-group-item active">Menu</li>
+							  	<li class="list-group-item"><a href="<?php
+echo site_url("user/profile"); ?>">Thông tin tài khoản</a></li>
+							  	<li class="list-group-item"><a href="<?php
+echo site_url("user/history"); ?>">Lịch sử đơn hàng</a></li>
+							 	<li class="list-group-item">Morbi leo risus</li>
+							</ul>
+                        </div>
