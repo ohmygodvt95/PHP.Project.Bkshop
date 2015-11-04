7c50daa58b251fe6f93e3600753d910b744a934e
Le Vinh Thien <ohmygodvt95@gmail.com>
2015-11-04 14:42:39 +0700

    update

--- application/config/routes.php
+++ application/config/routes.php
@@ -53,6 +53,7 @@ $route['default_controller'] = 'home';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 $route['san-pham/(:any)/(:any)'] = "product/index/$1/$2";
+$route['san-pham/(:any)'] = "product/index/$1";
 $route['san-pham/(:any)/(:any)/(:any)/(:any)'] = "product/index/$1/$2/$3/$4";
 $route['chi-tiet/(:any)'] = "product/details/$1";
 
